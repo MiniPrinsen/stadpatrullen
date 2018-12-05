528accdbc41bab8152af2240e36c2372b3d8bc08
id14mwn@cs.umu.se <martin.a.willman@gmail.com>
2018-12-05 16:46:45 +0100

    Removed API Key from repo

--- stadpatrullen/Firebase.js
+++ stadpatrullen/Firebase.js
@@ -1,8 +1,10 @@
 //<script src="https://www.gstatic.com/firebasejs/5.6.0/firebase.js"></script>
 import * as firebase from "firebase";
+import { FIREBASE_REACT_NATIVE_API_KEY } from 'react-native-dotenv'
+
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyD0QBRnvIJx5tDmuG94zGOD1OhCV9wFsSw",
+    apiKey: FIREBASE_REACT_NATIVE_API_KEY,
     authDomain: "stadpatrullen-a7783.firebaseapp.com",
     databaseURL: "https://stadpatrullen-a7783.firebaseio.com",
     projectId: "stadpatrullen-a7783",
